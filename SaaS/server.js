4bf9d6e485248ac7bdfce2624b2509c9ce7e83ef
cucbui57 <cucbui57@gmail.com>
2017-08-30 00:27:27 +0700

    Homework nodejs

--- SaaS/server.js
+++ SaaS/server.js
@@ -1,26 +1,78 @@
 const express = require('express');
+const fileController = require('./fileController');
+const filename = 'test.txt';
 
 let app = express();
 
-// app.get('/', (req, res) => {
-//   res.send('Hello, is it me you looking for');
-// });
-
-// app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) =>{
-  console.log(__dirname);
-  res.sendFile(__dirname + '/public/index.html');
-});
-
-app.get('/style.css', (req, res) => {
-  res.sendFile(__dirname + '/public/style.css');
+  res.sendFile(__dirname + '/public/homework.html');
 });
 
 app.get('/about',(req, res) => {
-  res.send(`<h1>Cuc<h1>`);
+  res.sendFile(__dirname + '/public/cv.html')
 });
 
+app.get('/readFile', (req, res) => {
+  fileController.readFile(filename, (data) => {
+    res.send(`<html>
+      <head>
+      <style>
+        .menu{
+          background-color: #D5B0AC;
+          color: #402E2A;
+          font-weight: bold;
+          text-align: center;
+        }
+
+        span:hover{
+          background: #9CD08F;
+          color: #FFFFFF;
+          cursor: pointer;
+        }
+
+        .menu_horizontal{
+          /*height: 37px;*/
+          position: fixed;
+          width: 100%;
+        }
+
+        .menu_horizontal span{
+          display: inline-flex;
+          align-items: center;
+          justify-content: center;
+          width: 100px;
+          height: 30px;
+        }
+
+        a{
+          color: black;
+          text-decoration: none;
+        }
+
+        p{
+          padding-top: 5%;
+          text-align: center;
+          font-weight: bold;
+        }
+      </style>
+
+      </head>
+      <body>
+        <nav class="menu menu_horizontal" id="">
+          <span><a href=""> Trang chủ</a></span>
+          <span><a href="about"> About me</a></span>
+          <span><a href="readFile"> Read File</a></span>
+        </nav>
+        <p>${data}</p>
+      </body>
+    </html>
+    `)
+  });
+});
+
+
 app.listen(6969, () => {
-  console.log('server is up');
+  console.log('Server is up');
 });
